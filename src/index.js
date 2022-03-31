ed457a422a38f1617c11b108516a37e3ecbb531b
Rafsun Masud <rafsun82@gmail.com>
2022-03-31 11:45:08 -0400

    Add sections and sorting

--- src/index.js
+++ src/index.js
@@ -3,4 +3,21 @@ import menu from "./menu";
 
 const menuItems = menu.items;
 
+/* each key is a 'type' containing array of items of that type  */
+const sections = {};
+
+/* categorizes by types */
+menuItems.forEach((item) => {
+	// adds a new category
+	if (!(item.type in sections)) sections[item.type] = [];
+
+	// adds the element to the category
+	sections[item.type].push(item);
+});
+
+/* sorts each section by menuOrder */
+Object.keys(sections).forEach((type) => {
+	sections[type].sort((item1, item2) => item1.menuOrder - item2.menuOrder);
+});
+
 console.log(menuItems);
